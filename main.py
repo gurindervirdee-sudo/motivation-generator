efc573afba147a10c6035236842ab8aed21b487a
Gurinder <gs1234gurindersingh@gmail.com>
2026-03-26 17:49:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "Discipline beats motivation.",
+    "Stay consistent, results will follow.",
+    "Winners focus on winning.",
+    "Small steps daily = big results.",
+    "You either win or you learn."
+]
+
+print("\n🔥 Your Motivation for Today:\n")
+print(random.choice(quotes))
