ea9c878a03fc73971fcc8009ac2cca2245388acc
Gurinder <gs1234gurindersingh@gmail.com>
2026-03-26 18:19:53 +0530

    Added user input feature

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ quotes = [
     "Stay consistent, results will follow.",
     "Winners focus on winning.",
     "Small steps daily = big results.",
-    "You either win or you learn."
+    "You either win or you learn.",
+    "Focus on progress, not perfection.",
+    "Your habits define your future."
 ]
 
-print("\n🔥 Your Motivation for Today:\n")
+print("\n🔥 Motivation Generator 🔥")
+
+name = input("\nEnter your name: ")
+
+print(f"\nHey {name}, here's your motivation:\n")
 print(random.choice(quotes))
